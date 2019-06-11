33140546e6dd410e0fc2608ab50aeb7c8e26bfc3
liupeiyu <lvegod@163.com>
2019-06-11 19:43:18 +0800

    submit code

---
+++ StringSubject/SubjectOne/Subject.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-06-11 19:34
+# @Author  : peiyu
+# @Email   : lvegod@163.com
+# @File    : Subject.py
+# @Software: PyCharm
+'''
+
+echo "# algorithm" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:liupeiyu/algorithm.git
+git push -u origin master
+
+
+or
+
+git remote add origin git@github.com:liupeiyu/algorithm.git
+git push -u origin master
+
+
+'''
+class Subject(object):
+
---
+++ StringSubject/SubjectOne/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-06-11 19:34
+# @Author  : peiyu
+# @Email   : lvegod@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
